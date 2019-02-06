23343417643ea2a0104f4909c7449c1b80740c75
Kizza Naume <naomekizzan@gmail.com>
2019-02-07 00:29:38 +0300

    ft(create-react-project)Set up an empty react project.
    - Create a blank react project from scratch using Webpack 4 and Babel.
    
    [#163750657]

---
+++ config/webpack.base.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Add this in top
+
+const APP_DIR = path.resolve(__dirname, '../src'); // <===== new stuff added here
+
+module.exports = env => {
+  const { PLATFORM, VERSION } = env;
+  return merge([
+    {
+      entry: ['@babel/polyfill', APP_DIR], // <===== new stuff added here
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+
+          {
+            test: /\.scss$/,
+            use: [
+              PLATFORM === 'local'
+                ? MiniCssExtractPlugin.loader
+                : 'style-loader',
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.(png|jpg|gif|svg)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {}
+              }
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: './index.html'
+        }),
+        new webpack.DefinePlugin({
+          'process.env.VERSION': JSON.stringify(env.VERSION),
+          'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+        }),
+        new CopyWebpackPlugin([{ from: 'src/static' }]) // Add this in the plugins section
+      ],
+      resolve: { extensions: ['*', '.js', '.jsx'] }
+    }
+  ]);
+};
